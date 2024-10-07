e9737b466e18af98287000dfe2e230318b3c3089
Facundo Tucker <facutucker05@gmail.com>
2024-10-07 19:20:11 -0300

    implementacion class Vehiculo

--- actividadRegistroAutomotor/main.ts
+++ actividadRegistroAutomotor/main.ts
@@ -1,41 +1,20 @@
+import { Auto } from "./Auto";
+import { Moto } from "./Moto";
+import { Camion } from "./Camion";
 import { Rueda } from "./Rueda";
 import { RegistroAutomotor } from "./RegistroAutomotor";
 
-const ruedasPrimerAuto : Rueda[] = [
-    new Rueda(16),
-    new Rueda(16),
-    new Rueda(16),
-    new Rueda(16),
-]
-
-const ruedasSegundoAuto : Rueda[] = [
-    new Rueda(15.5),
-    new Rueda(15.5),
-    new Rueda(15.5),
-    new Rueda(15.5),
-]
-
-const ruedasPrimerMoto : Rueda[] = [
-    new Rueda(17),
-    new Rueda(17),
-]
-
-const ruedasPrimerCamion : Rueda[] = [
-    new Rueda(20),
-    new Rueda(20),
-    new Rueda(20),
-    new Rueda(20),
-    new Rueda(20),
-    new Rueda(20),
-]
-
+const ruedasPrimerAuto: Rueda[] = [new Rueda(16), new Rueda(16), new Rueda(16), new Rueda(16)];
+const ruedasSegundoAuto: Rueda[] = [new Rueda(15.5), new Rueda(15.5), new Rueda(15.5), new Rueda(15.5)];
+const ruedasPrimerMoto: Rueda[] = [new Rueda(17), new Rueda(17)];
+const ruedasPrimerCamion: Rueda[] = [new Rueda(20), new Rueda(20), new Rueda(20), new Rueda(20), new Rueda(20), new Rueda(20)];
 
 const registroAutomotor : RegistroAutomotor = new RegistroAutomotor("Facundo SA");
 
-registroAutomotor.agregarAuto("Volswagen", "Suran", ruedasPrimerAuto,"AC390ZZ");
-registroAutomotor.agregarAuto("Ford", "Fiesta", ruedasSegundoAuto,"B222222");
-registroAutomotor.agregarMoto("Honda", "Hornet", "Azul plateado", ruedasPrimerMoto,"ACT999");
-registroAutomotor.agregarCamion("Iveco", "Tector", 5000, ruedasPrimerCamion,"ZZ456CC");
+registroAutomotor.agregarAuto(new Auto("Volswagen", "Suran", ruedasPrimerAuto,"AC390ZZ"));
+registroAutomotor.agregarAuto(new Auto("Ford", "Fiesta", ruedasSegundoAuto,"B222222"));
+registroAutomotor.agregarMoto(new Moto("Honda", "Hornet", "Azul plateado", ruedasPrimerMoto,"ACT999"));
+registroAutomotor.agregarCamion(new Camion("Iveco", "Tector", 5000, ruedasPrimerCamion,"ZZ456CC"));
 
 console.log(registroAutomotor.getNombre() + "\n");      //get registro
 
@@ -44,11 +23,11 @@ registroAutomotor.eliminarAuto("B222222");              //auto
 registroAutomotor.consultarAuto("B222222");
 
 console.log(registroAutomotor.getListaMotos());
-//registroAutomotor.eliminarMoto("ACT999");               //moto
+registroAutomotor.eliminarMoto("ACT999");               //moto
 registroAutomotor.consultarMoto("ACT999");
 
 console.log(registroAutomotor.getListaCamiones());
-//registroAutomotor.eliminarCamion("ZZ456CC")             //camion
+registroAutomotor.eliminarCamion("ZZ456CC")             //camion
 registroAutomotor.consultarCamion("ZZ456CC")
 
 registroAutomotor.modificarAuto("AC390ZZ", "Toyota", "Gol", ruedasPrimerAuto);   //modificar auto
@@ -58,6 +37,6 @@ registroAutomotor.modificarMoto("ACT999", "Yamaha", "", "Rojo", ruedasPrimerMoto
 registroAutomotor.consultarMoto("ACT999");
 
 registroAutomotor.modificarCamion("ZZ456CC", "NuevoIveco", "Carga", 30000, ruedasPrimerCamion)      //modificar camion
-registroAutomotor.consultarCamion("ZZ456CC")
+registroAutomotor.consultarCamion("ZZ456CC");
 
 
